31f85e1cd207ced106ab72bc94d886d28093f6e0
zck0423 <591193653@qq.com>
2020-05-02 22:24:27 +0800

    个人页加背景图

--- app/src/main/java/moe/sui/unimarket/PersonalPageActivity.java
+++ app/src/main/java/moe/sui/unimarket/PersonalPageActivity.java
@@ -1,8 +1,12 @@
 package moe.sui.unimarket;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView;
+import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView;
 
 public class PersonalPageActivity  extends AppCompatActivity  {
     @Override
@@ -11,7 +15,13 @@ public class PersonalPageActivity  extends AppCompatActivity  {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_page);
+        //QMUIGroupListView groupListView = findViewById(R.id.grouplistview);
 
+        Context context=getApplicationContext();
+        QMUIGroupListView item1 = new QMUIGroupListView(context);
+        QMUIGroupListView.Section setion=QMUIGroupListView.newSection(context);
+      //  setion.setTitle("喜欢列表");
+      //  setion.addTo(item1);
 
     }
 }
